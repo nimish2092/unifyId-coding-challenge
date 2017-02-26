a05ae2cd1b7caa78355e6c6666f401407559f2ca
Nimish Unde <nimish.2092@gmail.com>
2017-02-25 20:48:44 -0800

    refactor code

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-var random = require("random");
-var ImageJS = require("imagejs");
+var random = require("random");                                     // nodejs package for random.org client
+var ImageJS = require("imagejs");                                   
 var counter = 0;
 var input = [];
-//var integers;
-var bitmap = new ImageJS.Bitmap({width: 128, height: 128});
+var bitmap = new ImageJS.Bitmap({width: 128, height: 128});         // creates an empty bitmap of size 128*128
 function randomCallback(integers){
-// Prints row 0, column 0
-    //console.log(integers);
-    for(var m = 0; m < 1000; m++){
+
+    for(var m = 0; m < 1000; m++){                                  // create duplicates of random values to fill up bit map
         for(var k = 0; k < integers.length; k++){
             input[counter] = integers[k];
             counter++;
         }
     }
-    //console.log(input.length);
     counter = 0;
+    
     for(var i = 0 ; i < 128; i++){
-        for (var j = 0; j < 128; j++) {
+        for (var j = 0; j < 128; j++) {                             // set each pixel of a bitmap to a random value
             var temp = integers[counter];
             var color = {r: temp, g: temp, b: temp};
             bitmap.setPixel(i,j,color);
@@ -39,7 +37,7 @@ function errorCallback(type,code,string){
 random.generateIntegers(randomCallback,options,errorCallback);
 
 
-return bitmap.writeFile("image.jpg", { quality:90 })
-    .then(function() {
+return bitmap.writeFile("image.jpg", { quality:90 })            // write the bitmap on the disk with name "image.jpg"
+    .then(function() {                  
         // bitmap has been saved 
 });
