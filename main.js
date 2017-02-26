fafdd114e47f23377926f93c8fe272fc8298414e
Nimish Unde <nimish.2092@gmail.com>
2017-02-25 20:24:26 -0800

    improvements

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 var random = require("random");
 var ImageJS = require("imagejs");
+var counter = 0;
+//var integers;
+var bitmap = new ImageJS.Bitmap({width: 128, height: 128});
 function randomCallback(integers){
 // Prints row 0, column 0
     console.log(integers);
+    /*for(var i = 0 ; i < 10; i++){
+        for (var j = 0; j < 10; j++) {
+            bitmap.setPixel(i,j, integers[++counter],integers[++counter],integers[++counter],integers[++counter]);
+        }
+    }*/
 }
 var options = {
 	secure: true, // Make the request secure
@@ -18,7 +26,7 @@ function errorCallback(type,code,string){
 }
 random.generateIntegers(randomCallback,options,errorCallback);
 
-var bitmap = new ImageJS.Bitmap({width: 128, height: 128});
+
  
 // Create a bitmap filled with green 
 //var greenBitmap = new ImageJS.Bitmap({width: 100, height: 100, color: {r: 255, g: 255, b: 255, a: 255});
@@ -36,7 +44,10 @@ var bitmap = new ImageJS.Bitmap({width: 128, height: 128});
 // Create an empty (null) bitmap, ready for reading from file or stream 
 //var nullBitmap = new ImageJS.Bitmap();
 //console.log(bitmap);
-return bitmap.writeFile("image.png", { quality:75 })
+
+
+
+return bitmap.writeFile("image.jpg", { quality:75 })
     .then(function() {
         // bitmap has been saved 
 });
